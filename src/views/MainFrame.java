186c184a5ba7527142ca65a837b613bae06ae571
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2022-01-06 13:40:28 +0000

    fix layout

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -286,7 +286,7 @@ public class MainFrame extends javax.swing.JFrame {
         jpnContainerLayout.setVerticalGroup(
             jpnContainerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jpnMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jpnBean, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jpnBean, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         jpnBean.getAccessibleContext().setAccessibleName("Thu Tiền");
